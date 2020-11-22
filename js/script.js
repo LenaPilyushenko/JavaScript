2679dc0cdefd8bafa3a179cd1f20b1b0876f21e7
Lena Pilyushenko <elena_pilyushenko@mail.ru>
2020-11-22 18:25:04 +0300

    feat(script.js): add feat

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-let money, income, addExpenses, deposit, mission, period;
+let money = 40000;
+const income = 'фриланс';
+let  addExpenses = 'Коммуналка, интернет, бензин, кредит, ипотека';
+let deposit = true;
+let mission = 500000;
+const  period = 12;
 
-alert ('Это сообщение с любым текстом');
+console.log ('money : ' + typeof money );
+
+console.log ('money : ' + typeof income );
+
+console.log ('deposit : ' + typeof deposit );
+
+console.log ('Длина строки addExpenses : ' + addExpenses.length );
+
+console.log ('Период равен ' + period + ' месяцев');
+
+console.log ('Цель заработать ' + mission + ' рублей');
+
+let newAddExpenses = addExpenses.toLowerCase();
+
+console.log (newAddExpenses.split(', '));
+
+let budgetDay = money/30;
+
+console.log ('budgetDay = ' + budgetDay );
