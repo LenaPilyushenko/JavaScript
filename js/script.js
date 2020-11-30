c287e90c481c123afcc0f6fa2a81c5adfba8e3d9
Lena Pilyushenko <elena_pilyushenko@mail.ru>
2020-11-30 18:15:53 +0300

    feat(js/script.js):new feat

--- js/script.js
+++ js/script.js
@@ -21,6 +21,8 @@ let appData = {
     expenses: {},
     addExpenses: [],
     deposit: false,
+    persentDeposit: 0,
+    moneyDeposit: 0,
     mission: 50000,
     period: 12,
     budget: money,
@@ -28,16 +30,39 @@ let appData = {
     budgetMonth: 0,
     expensesAmonth: 0,
     asking: function() {
-        const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-        appData.addExpenses = addExpenses.toLowerCase().split(', ');
+        let cashIncome,
+            itemIncome;
+        if (confirm('Есть ли у вас испточник дополнительного заработка?')) {
+
+            do {
+                itemIncome = prompt('Какой у вас дополнительный заработок?' , 'Таксую');
+            }
+            while (!isNaN(itemIncome));
+
+            do {
+                cashIncome = prompt('Сколько вы на этом зарабатываете в месяц?', 10000);
+            }
+            while (!isNumber(cashIncome));
+
+            appData.income[itemIncome] = cashIncome;
+        }
         appData.deposit = confirm('Есть ли у вас депозит в банке?');
+
+        let addExpenses;
+        do {
+            addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+        }
+        while (!isNaN(addExpenses));
+       
+        appData.addExpenses = addExpenses.toLowerCase().split(', ');
+                
         let key = 0,
             amount, 
             sum = 0 ;
         for (let i = 0; i < 2; i++ ) {
     
             key = prompt('Введите обязательную статью расходов?');
-             do {
+            do {
                 amount = +prompt('Во сколько это обойдется?');
             }
             while (!isNumber(amount) );
@@ -71,6 +96,21 @@ let appData = {
     getTargetMonth: function () {
         return (Math.ceil(appData.mission / appData.budgetMonth));
     },
+    getInfoDeposit: function() {
+        if (appData.deposit) {
+            do {
+                appData.persentDeposit = prompt('Какой годовой процент' , '10');
+            }
+            while (!isNumber(appData.persentDeposit));
+            do {
+               appData.moneyDeposit = prompt('Какая сумма заложена', 10000);
+            }
+            while (!isNumber(appData.moneyDeposit));
+        }
+    },
+    calcSavedMoney: function() {
+        return appData.budgetMonth * appData.period;
+    }
 };
 
 appData.asking();
@@ -90,8 +130,17 @@ console.log(appData.getStatusIncome());
 console.log('Наша программа включает в себя данные: ');
 
 for (let key in appData) {
-    // этот код будет вызван для каждого свойства объекта
-    // ..и выведет имя свойства и его значение
-  
     console.log(key + " : " + appData[key] );
 }
+
+for (let value of appData.addExpenses) {
+    let myData =(value.charAt(0).toUpperCase()) + value.substr(1).toString();
+    console.log(myData);
+}
+
+const arr = appData.addExpenses.map(function(elem) {
+    const str = elem.trim(); 
+    console.log(str);
+    return (str[0].toUpperCase() + str.slice(1));
+});
+console.log(arr.toString());
